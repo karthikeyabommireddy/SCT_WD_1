7f7d1364ac9566306bf0eeb6cee6a3e70ced513e
KARTHIKEYA REDDY BOMMIREDDY <70801276+karthikeyabommireddy@users.noreply.github.com>
2025-01-14 04:58:04 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+function showSection(sectionId) {
+  // Hide all sections
+  document.querySelectorAll('.section').forEach(section => {
+    section.classList.remove('active');
+  });
+  
+  // Show the selected section
+  document.getElementById(sectionId).classList.add('active');
+}
